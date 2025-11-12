6abedcedd2cef6525ca6e7a36ea6f0a36c645642
maurmni <ylavartalo@outlook.com>
2025-11-12 15:52:20 +0500

    22

---
+++ WebAPI/Migrations/20251112104817_Cool-Updates-FR.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class CoolUpdatesFR : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "DailyPrice",
+                table: "Cars",
+                type: "numeric",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DailyPrice",
+                table: "Cars");
+        }
+    }
+}
